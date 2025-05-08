6c9914f487cdfce1c47c5992d6e12a46c14c1d87
nsadineni@google.com <nsadineni@google.com>
2025-05-08 15:46:17 +0000

    git ignore change

--- sample-data/terraform/backend.tf
+++ sample-data/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "<TERRAFORM_BUCKET>"
-    prefix = "<TERRAFORM_ENV>"
+    bucket = "aef-hackathotest-tfe"
+    prefix = "sample-data/environments/dev"
   }
 }
